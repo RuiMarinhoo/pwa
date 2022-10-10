f593bc1062bafefd7169d09bf42ac98669678136
RuiMarinhoo <ruijorgemm@hotmail.com>
2022-10-10 17:59:16 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/MyCB'));
+app.use(express.static(__dirname + '/dist/PWA'));
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+
-    '/dist/my-cb/index.html'));});
+    '/dist/pwa/index.html'));});
 app.listen(process.env.PORT || 8080);
